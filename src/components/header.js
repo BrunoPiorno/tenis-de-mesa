d52a1e8b647192953e8549ee1ab0f6e89bd645b6
Version Update <48295197+BrunoPiorno@users.noreply.github.com>
2024-05-17 16:27:44 -0300

    mobile

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,44 @@
-import React from 'react';
+// Header.js
+import React, { useState } from 'react';
 import './header.css'; 
 import logo from '../images/logo-posta.png'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import WhatsApp from './whatsapp'; 
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <div className="container">
-        <div className="logo"><img src={logo} alt="Logo"/></div>
-        <nav>
-          <ul className="menu">
-            <li><a href="#categorias" className="menu-link">Categorias</a></li>
-            <li><a href="#sponsors" className="menu-link">Sponsors</a></li>
-            <li><a href="#inscripcion" className="menu-link">Inscripción</a></li>
-          </ul>
-          <div className="social-icons">
-            <a href="https://www.instagram.com/trenque.tdm/" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faInstagram} className="icon instagram-icon" />
-            </a>
-            <a href="https://www.facebook.com/trenque.tdm?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faFacebook} className="icon facebook-icon" />
-            </a>
-            <WhatsApp /> 
+        <div className="header__cont">
+          <div className="logo"><img src={logo} alt="Logo"/></div>
+          <div className="menu-icon" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
           </div>
-        </nav>
+          <nav className={`nav ${menuOpen ? 'open' : ''}`}>
+            <ul className="menu">
+              <li><a href="#categorias" className="menu-link">Categorias</a></li>
+              <li><a href="#sponsors" className="menu-link">Sponsors</a></li>
+              <li><a href="#inscripcion" className="menu-link">Inscripción</a></li>
+            </ul>
+            <div className="social-icons">
+              <a href="https://www.instagram.com/trenque.tdm/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} className="icon instagram-icon" />
+              </a>
+              <a href="https://www.facebook.com/trenque.tdm?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faFacebook} className="icon facebook-icon" />
+              </a>
+              <WhatsApp /> 
+            </div>
+          </nav>
+        </div>
       </div>
     </header>
   );
