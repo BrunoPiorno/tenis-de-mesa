acfb60d219950964f204c9b25951877fad2ace03
Version Update <48295197+BrunoPiorno@users.noreply.github.com>
2024-05-21 14:09:05 -0300

    cosas

--- src/layout/hero.js
+++ src/layout/hero.js
@@ -7,11 +7,11 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero_image">
-      <img src={pingpongImage} alt="Imagen de Hero" className="hero-image" /> 
+        <img src={pingpongImage} alt="Imagen de Hero" className="hero-image" /> 
       </div>
       <div className="hero-content">
         <h1 className="hero-title">TENIS DE MESA <br/> TRENQUE LAUQUEN</h1>
-        <button><a href="link-externo">Preinscripción Al Torneo</a></button>
+        <button><a href="#inscripcion">Preinscripción Al Torneo</a></button>
       </div>
     </section>
   );
