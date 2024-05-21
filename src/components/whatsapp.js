32b90867fd362f8b97c9aadfa4397461fd7db2b2
Version Update <48295197+BrunoPiorno@users.noreply.github.com>
2024-05-21 14:27:56 -0300

    cosas

--- src/components/whatsapp.js
+++ src/components/whatsapp.js
@@ -3,10 +3,10 @@ import '../style/whatsapp.css';
 import whatsapp from '../images/whatsapp.png'; 
 
 const WhatsappIcon = () => {
-  const whatsappNumber = '+542392460230'; 
+  const whatsappNumber = '+542392484143';
   
   return (
-    <a href={`https://api.whatsapp.com/send?phone=${whatsappNumber}`} className="whatsapp-icon" target="_blank" rel="noopener noreferrer">
+    <a href={`https://wa.me/${whatsappNumber}`} className="whatsapp-icon" target="_blank" rel="noopener noreferrer">
       <img src={whatsapp} alt="Whatsapp" className="whatsapp" /> 
     </a>
   );
