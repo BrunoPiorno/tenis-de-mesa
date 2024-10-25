4047422e005e765d48d4b0b86ec5517f1d7b9213
Bruno Piorno <brunopiornop@gmail.com>
2024-10-25 17:30:27 -0300

    Agrega cambios en Hero.js y hero.css para imágenes responsive

--- src/layout/hero.js
+++ src/layout/hero.js
@@ -1,13 +1,15 @@
 // Hero.js
 import React from 'react';
 import pingpongImage from '../images/test.png'; 
+import mobile from '../images/mobile.jpeg'; 
 import './hero.css';
 
 const Hero = () => {
   return (
     <section className="hero">
       <div className="hero_image">
-        <img src={pingpongImage} alt="Imagen de Hero" className="hero-image" /> 
+        <img src={pingpongImage} alt="Imagen de Hero" className="hero-image desktop" /> 
+        <img src={mobile} alt="Imagen de Hero" className="hero-image mobile" /> 
       </div>
     </section>
   );
